cc1e62b90d0fc664a651a5da714f40a26b0ef397
Lukas Antesberger <lukas.antesberger@produktmacher.com>
2021-01-23 16:09:51 +0100

    add home config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
-import config from '../../config.json'
+import config from '../../general_config/config.json'
 
 type Config = {
   readonly base_url: string
---
+++ src/lib/home.ts
@@ -0,0 +1,8 @@
+import home from '../../general_config/home.json'
+
+type Home = {
+  readonly intro_headline: string
+  readonly intro_text: string
+}
+
+export default home as Home
