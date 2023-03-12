00011208feba35d3686fb1b2ca30cca78f17f03c
Lukas Antesberger <lukas.antesberger@pina.earth>
2023-03-12 18:59:12 +0100

    fix mdx build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
-module.exports = {
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   pageExtensions: ['mdx', 'tsx'],
   webpack: (config) => {
     config.module.rules.push(
@@ -17,3 +31,5 @@ module.exports = {
     return config
   },
 }
+
+module.exports = withMDX(nextConfig)
